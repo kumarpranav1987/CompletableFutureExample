d7a01b147ba4b432d1935f8ce82a0ad2b8b6e57d
pranav.kumar@iongroup.com <pranav.kumar@iongroup.com>
2019-05-29 12:32:04 +0530

    Call Blocking API in Non Blocking Way

--- src/main/java/com/livetechstudy/CompletableFutureExample.java
+++ src/main/java/com/livetechstudy/CompletableFutureExample.java
@@ -1,6 +1,10 @@
 package com.livetechstudy;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,7 +14,16 @@ public class CompletableFutureExample {
 
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		logger.info("Main Started");
-		
+		Shop s = new Shop();
+		ExecutorService es = Executors.newCachedThreadPool();
+		Future<QuotedPricePrice> quotedPrice = es.submit(new Callable<QuotedPricePrice>() {
+			@Override
+			public QuotedPricePrice call() throws Exception {
+				return s.getPrice("XYZ");
+			}
+		});
+		someOtherWork();
+		logger.info(quotedPrice.get().toString());//This is blocking
 	}
 
 	private static void someOtherWork() {
