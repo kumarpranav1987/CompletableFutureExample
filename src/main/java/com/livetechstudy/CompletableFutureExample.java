b84da5dbda8f28da86e802be4c54dc4beee05908
pranav.kumar@iongroup.com <pranav.kumar@iongroup.com>
2019-05-29 15:19:34 +0530

    Updated

--- src/main/java/com/livetechstudy/CompletableFutureExample.java
+++ src/main/java/com/livetechstudy/CompletableFutureExample.java
@@ -1,34 +1,53 @@
 package com.livetechstudy;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 //https://gist.github.com/kumarpranav1987/1303e4baa4ca3df009ac59755eaa91ce
 public class CompletableFutureExample {
 	private static final Logger logger = LoggerFactory.getLogger(CompletableFutureExample.class);
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		logger.info("Main Started");
 		Shop s = new Shop();
-		ExecutorService es = Executors.newCachedThreadPool();
-		Future<QuotedPricePrice> quotedPrice = es.submit(new Callable<QuotedPricePrice>() {
-			@Override
-			public QuotedPricePrice call() throws Exception {
-				return s.getPrice("XYZ");
-			}
+		
+		//1.Below part is executed in different thread then main . Which thread pool?
+		/*CompletableFuture.supplyAsync(() -> {
+			return s.getPrice("XYZ");
+		}).thenAccept((quote) -> {
+			// thenAccept part is executed in same thread where supplyAsync is executed or in main thread if the supplyAsync part completes immediately
+			//Comment delay() call in calculatePrice to verify 
+			logger.info(quote.toString());
+		});*/
+		
+		
+		//2.Use your own executor for Completable future
+		
+		/*ExecutorService executor = Executors.newCachedThreadPool();
+		CompletableFuture.supplyAsync(() -> {
+			return s.getPrice("XYZ");
+		}, executor).thenAccept((price) -> {
+			logger.info(price.toString());
 		});
-		someOtherWork();
+		executor.shutdown();*/
+		
+		//3.Code to to show that thenAccept can run in different thread for of Common pool then the thread in which supplyAsync runs
+		/*CompletableFuture<QuotedPricePrice> qp = CompletableFuture.supplyAsync(() -> {
+			return s.getPrice("PQR");
+		});
 		try {
-			logger.info(quotedPrice.get().toString());//This is blocking
-		} catch (InterruptedException | ExecutionException e) {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		es.shutdown();
+		qp.thenAcceptAsync((price)->{logger.info(price.toString());});*/
+		someOtherWork();
+		
 	}
 
 	private static void someOtherWork() {
@@ -52,7 +71,7 @@ class Shop {
 
 	private QuotedPricePrice calculatePrice(String product) {
 		delay();
-		return new QuotedPricePrice(product, product.length()*100);
+		return new QuotedPricePrice(product, product.length() * 100);
 	}
 
 	private void delay() {
